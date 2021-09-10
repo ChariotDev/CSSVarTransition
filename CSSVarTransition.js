2e4dcfdd10075af33f499ca5ebfaf633e231805a
Jody Doolittle <doolijb@gmail.com>
2021-09-09 18:07:03 -0700

    initial

---
+++ CSSVarTransition.js
@@ -0,0 +1,48 @@
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function transitionRGB(variable, target, seconds = 1000, steps = 10) {
+    const rgb = getComputedStyle(document.documentElement)
+        .getPropertyValue(variable)
+        .split(",")
+        .filter((x) => x.trim().length && !isNaN(x))
+        .map(Number);
+
+    let red = rgb[0];
+    let green = rgb[1];
+    let blue = rgb[2];
+
+    if (!Number.isInteger(red) || !Number.isInteger(green) || !Number.isInteger(blue)) {
+        throw `Values [${red}, ${green}, ${blue}] are illegal, please integers only. Suggest 0 - 255.`
+        return;
+    }
+
+    const redTarg = target[0];
+    const greenTarg = target[1];
+    const blueTarg = target[2];
+    const stepTime = seconds / steps;
+    console.log(stepTime)
+    let loops = 0;
+    const redStep = Math.floor((redTarg - red) / steps)
+    const greenStep = Math.floor((greenTarg - green) / steps)
+    const blueStep = Math.floor((blueTarg - blue) / steps)
+
+    console.log(redStep)
+
+    while (loops < steps) {
+        loops += 1
+        red += redStep
+        green += greenStep
+        blue += blueStep
+        document.body.style.setProperty(
+            variable, `${red}, ${green}, ${blue}`
+        );
+        await delay(stepTime);
+    }
+
+    document.body.style.setProperty(
+        variable, `${redTarg}, ${greenTarg}, ${blueTarg}`
+    );
+}
+
+export default transitionRGB
